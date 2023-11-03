5932574e7f9a0836d929ea0b795cb3a14b5c4ef4
nkte8 <nkte8@outlook.jp>
2023-11-04 02:31:08 +0900

    astro-relative-linksを導入・ビルド

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
+import relativeLinks from 'astro-relative-links';
 
 // https://astro.build/config
 export default defineConfig({
-    outDir: './docs'
+    outDir: './docs',
+    site: 'https://nkte8.github.io/astro-practice/',
+    integrations: [relativeLinks()]
 });
